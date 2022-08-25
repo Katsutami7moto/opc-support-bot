ee00184ab5f5d99f3079e7bcb834d10fb422f69f
Katsutami7moto <nclsf87@gmail.com>
2022-08-25 20:30:26 +0300

    Setup echo bot for VK

--- vk_bot.py
+++ vk_bot.py
@@ -1,10 +1,11 @@
 import logging
+import random
 
 from environs import Env
 
 from dialogflow import reply_by_intent
 
-import vk_api
+import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 logging.basicConfig(
@@ -15,6 +16,14 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1,1000)
+    )
+
+
 def main():
     env = Env()
     env.read_env()
@@ -22,18 +31,13 @@ def main():
     project_id = env('PROJECT_ID')
     language_code = env('LANGUAGE_CODE')
 
-    vk_session = vk_api.VkApi(token=vk_club_token)
-
+    vk_session = vk.VkApi(token=vk_club_token)
+    vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
 
 
 if __name__ == '__main__':
