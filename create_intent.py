3560ea117e51035871795d6ed161b60e7aad5cad
Katsutami7moto <nclsf87@gmail.com>
2022-08-25 20:49:11 +0300

    Add DialogFlow to VK bot

--- create_intent.py
+++ create_intent.py
@@ -2,43 +2,8 @@ import argparse
 import json
 
 from environs import Env
-from google.cloud import dialogflow
 
-
-def create_intent(
-    project_id: str,
-    display_name: str,
-    training_phrases_parts: list[str],
-    message_texts: list[str]
-) -> dialogflow.Intent:
-
-    intents_client = dialogflow.IntentsClient()
-
-    parent = dialogflow.AgentsClient.agent_path(project_id)
-
-    training_phrases = []
-    for training_phrases_part in training_phrases_parts:
-        part = dialogflow.Intent.TrainingPhrase.Part(
-            text=training_phrases_part
-        )
-        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
-        training_phrases.append(training_phrase)
-
-    text = dialogflow.Intent.Message.Text(text=message_texts)
-    message = dialogflow.Intent.Message(text=text)
-
-    intent = dialogflow.Intent(
-        display_name=display_name,
-        training_phrases=training_phrases,
-        messages=[message]
-    )
-
-    intents_client.create_intent(
-        request={
-            'parent': parent, 
-            'intent': intent
-        }
-    )
+from dialogflow import create_intent
 
 
 def main():
--- vk_bot.py
+++ vk_bot.py
@@ -2,11 +2,10 @@ import logging
 import random
 
 from environs import Env
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 from dialogflow import reply_by_intent
-
-import vk_api as vk
-from vk_api.longpoll import VkLongPoll, VkEventType
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -16,11 +15,16 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def echo(event, vk_api):
-    vk_api.messages.send(
+def reply(event, vk_api_method, project_id, language_code):
+    vk_api_method.messages.send(
         user_id=event.user_id,
-        message=event.text,
-        random_id=random.randint(1,1000)
+        random_id=random.randint(1, 1000),
+        message=reply_by_intent(
+            project_id,
+            event.user_id,
+            event.text,
+            language_code
+        )
     )
 
 
@@ -31,13 +35,13 @@ def main():
     project_id = env('PROJECT_ID')
     language_code = env('LANGUAGE_CODE')
 
-    vk_session = vk.VkApi(token=vk_club_token)
-    vk_api = vk_session.get_api()
+    vk_session = vk_api.VkApi(token=vk_club_token)
+    vk_api_method = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            reply(event, vk_api_method, project_id, language_code)
 
 
 if __name__ == '__main__':
